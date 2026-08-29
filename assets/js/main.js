a856d9b34e352271c5211f8d70516595555086ef
nasalina120 <nasalina120@gmail.com>
2026-08-29 16:53:52 +0200

    Add display mobile operator on phone number hover

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,6 @@
 // 2. Підписка
 
-btnSub = document.querySelector(".btn-subscribe");
+const btnSub = document.querySelector(".btn-subscribe");
 
 btnSub.onclick = function changesubscribe(e) {
   if (btnSub.textContent === "Підписатися") {
@@ -14,8 +14,31 @@ btnSub.onclick = function changesubscribe(e) {
 
 // 3. Вподобання
 
-btnHeart = document.querySelector(".btn-heart");
+const btnHeart = document.querySelector(".btn-heart");
 
 btnHeart.addEventListener("click", () => {
   btnHeart.classList.toggle("btn-heartActive");
 });
+
+const phoneNumber = document.querySelector(".phoneNum");
+const originalNum = phoneNumber.textContent;
+phoneNumber.addEventListener("mouseenter", () => {
+  phoneNumber.textContent = " (Kyivstar) " + originalNum;
+});
+phoneNumber.addEventListener("mouseleave", () => {
+  phoneNumber.textContent = originalNum;
+});
+
+// 4. ✅ Додаткова інформація
+
+// Номер телефону
+// При наведенні на номер телефону (mouseenter) перед ним повинна з'являтися
+// назва мобільного оператора:
+// Kyivstar: +380 XX XXX XX XX
+// При відведенні курсору (mouseleave) назва оператора повинна зникати,
+// залишаючи лише номер телефону.
+// Примітка: визначайте мобільного оператора за кодом номера телефону.
+//  Можливість перенесення номера між операторами не враховуйте.
+
+// Дата народження
+// Додайте до дати народження атрибут title, у якому буде зазначено вік користувача.
